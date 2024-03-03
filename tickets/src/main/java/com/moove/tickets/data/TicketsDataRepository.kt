cb3f76f236c652db5f9651ca0a145743f796c11a
Fatih Kurçenli <rizelifatih5353@gmail.com>
2024-03-03 12:16:20 +0300

    yetki tanımlanması yapıldı.

--- tickets/src/main/java/com/moove/tickets/data/TicketsDataRepository.kt
+++ tickets/src/main/java/com/moove/tickets/data/TicketsDataRepository.kt
@@ -14,7 +14,6 @@ class TicketsDataRepository(
     private val backgroundDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : TicketsRepository {
 
-
     override suspend fun getRyders(): List<Ryder> = withContext(backgroundDispatcher) {
         val data = ticketsLocalDataSource.getData()
         data.map { it.value.asDomain(it.key) }
